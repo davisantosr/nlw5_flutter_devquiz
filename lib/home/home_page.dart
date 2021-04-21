e43503d53cc8c8dfeeb298427ddc7dd4a0cb7f79
davisantosr <davi-rocha2@hotmail.com>
2021-04-21 15:37:08 -0300

    home page screen

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:devquiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,14 +14,39 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBarWidget(),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: EdgeInsets.all(16),
+        child: Column(
           children: [
-            LevelButtonWidget(label: "Fácil"),
-            LevelButtonWidget(label: "Médio"),
-            LevelButtonWidget(label: "Difícil"),
-            LevelButtonWidget(label: "Perito")
+            SizedBox(
+              height: 24,
+            ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(label: "Fácil"),
+                  LevelButtonWidget(label: "Médio"),
+                  LevelButtonWidget(label: "Difícil"),
+                  LevelButtonWidget(label: "Perito")
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisSpacing: 8,
+                crossAxisCount: 2,
+                mainAxisSpacing: 8,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                ],
+              ),
+            )
           ],
         ),
       ),
--- lib/home/widgets/level_button/level_button_widget.dart
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -37,22 +37,19 @@ class LevelButtonWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 40),
-      child: Container(
-        decoration: BoxDecoration(
-          color: color,
-          border: Border.fromBorderSide(
-            BorderSide(color: borderColor),
-          ),
-          borderRadius: BorderRadius.circular(28),
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        border: Border.fromBorderSide(
+          BorderSide(color: borderColor),
         ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
-          child: Text(
-            label,
-            style: GoogleFonts.notoSans(color: fontColor, fontSize: 13),
-          ),
+        borderRadius: BorderRadius.circular(28),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+        child: Text(
+          label,
+          style: GoogleFonts.notoSans(color: fontColor, fontSize: 13),
         ),
       ),
     );
