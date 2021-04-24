05e1017a07ca5a40d6cce4c5c2717c6563c192f7
davisantosr <davi-rocha2@hotmail.com>
2021-04-24 19:07:37 -0300

    Button Challenge

--- lib/challenge/nextbutton/next_button_widget.dart
+++ lib/challenge/nextbutton/next_button_widget.dart
@@ -1,15 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:devquiz/core/app_colors.dart';
 
 class NextButtonWidget extends StatelessWidget {
   final String label;
+  final Color backgroundColor;
+  final Color fontColor;
+  final Color borderColor;
+  final VoidCallback onTap;
 
-  NextButtonWidget({required this.label});
+  NextButtonWidget(
+      {required this.label,
+      required this.backgroundColor,
+      required this.fontColor,
+      required this.borderColor,
+      required this.onTap});
+
+  // named constructor
+  NextButtonWidget.green({
+    required String label,
+    required VoidCallback onTap,
+  })   : this.backgroundColor = AppColors.darkGreen,
+        this.fontColor = AppColors.white,
+        this.borderColor = AppColors.green,
+        this.label = label,
+        this.onTap = onTap;
+
+  NextButtonWidget.white({
+    required String label,
+    required VoidCallback onTap,
+  })   : this.backgroundColor = AppColors.white,
+        this.fontColor = AppColors.grey,
+        this.borderColor = AppColors.border,
+        this.onTap = onTap,
+        this.label = label;
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {},
-      child: Text(label),
+    return Container(
+      height: 48,
+      child: TextButton(
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(backgroundColor),
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10))),
+            side: MaterialStateProperty.all(BorderSide(
+              color: borderColor,
+            ))),
+        onPressed: onTap,
+        child: Text(
+          label,
+          style: GoogleFonts.notoSans(
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+            color: fontColor,
+          ),
+        ),
+      ),
     );
   }
 }
